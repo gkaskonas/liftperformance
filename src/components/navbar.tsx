84f4cb8db00e81125e9da993a8bb7587719cf36d
Giedrius Kaskonas <pkpersonal@protonmail.com>
2023-05-08 15:38:40 +0800

    update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,11 +2,10 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function NavBar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
+  const [showMe, setShowMe] = useState(false);
+  function toggle() {
+    setShowMe(!showMe);
+  }
 
   return (
     <header
@@ -15,12 +14,15 @@ export default function NavBar() {
     >
       <section className="mx-auto flex max-w-4xl items-center justify-between p-4">
         <h1 className="text-3xl font-medium">
-          <Link href="/">🏋️ LiftPerformance</Link>
+          <Link href="/" className="tracking-wide">
+            🏋️ LiftPerformance
+          </Link>
         </h1>
         <div>
           <button
-            id="mobile-open-button"
+            id="hamburger-button"
             className="text-3xl focus:outline-none sm:hidden"
+            onClick={toggle}
           >
             &#9776;
           </button>
@@ -39,9 +41,39 @@ export default function NavBar() {
             </Link>
           </nav>
         </div>
-        {/* <div className="flex items-center justify-between max-w-4xl mx-auto p-4 text-xl">
-          <Link href="/">Home</Link>
-        </div> */}
+      </section>
+      <section
+        id="mobile-menu"
+        onClick={toggle}
+        style={{ display: showMe ? "block" : "none" }}
+        className="top-68 justify-content-center animate-open-menu absolute hidden w-full origin-top flex-col bg-black text-5xl"
+      >
+        <nav
+          className="flex min-h-screen flex-col items-center py-8"
+          aria-label="mobile"
+        >
+          <Link href="/" className="w-full py-6 text-center hover:opacity-90">
+            Home
+          </Link>
+          <Link
+            href="#team"
+            className="w-full py-6 text-center hover:opacity-90"
+          >
+            Meet the Team
+          </Link>
+          <Link
+            href="#booking"
+            className="w-full py-6 text-center hover:opacity-90"
+          >
+            Booking
+          </Link>
+          <Link
+            href="#contact"
+            className="w-full py-6 text-center hover:opacity-90"
+          >
+            Contact
+          </Link>
+        </nav>
       </section>
     </header>
   );
