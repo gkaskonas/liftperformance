6e18334d90760dc1f9bfc4b3eb3615c4b24c08a7
Giedrius Kaskonas <pkpersonal@protonmail.com>
2023-05-11 14:08:03 +0800

    fix stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import { Metadata } from "next";
+import Head from "next/head";
 import "./styles/globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Lift Performance | Personal Training | Hong Kong Island",
   description:
     "At Lift Performance, our elite coaches have one goal in mind - to elevate every aspect of your health and fitness",
@@ -15,6 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="canonical"
+          href="https://liftperformance.vercel.app/"
+          key="canonical"
+        />
+      </Head>
       <body>{children}</body>
     </html>
   );
