30a8bb6a76a887ab19c54b58c5c279088cb90f77
Giedrius Kaskonas <pkpersonal@protonmail.com>
2023-05-12 08:10:10 +0800

    fix: upgrade to 13.4.2

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,32 +1,32 @@
-// import { ImageResponse } from "next/server";
+import { ImageResponse } from "next/server";
 
-// export const alt = "Lift Performance";
-// export const size = {
-//   width: 1200,
-//   height: 630,
-// };
-// export const contentType = "image/png";
-// export const runtime = "edge";
+export const alt = "Lift Performance";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+export const contentType = "image/png";
+export const runtime = "edge";
 
-// export default function og() {
-//   return new ImageResponse(
-//     (
-//       <div
-//         style={{
-//           fontSize: 128,
-//           background: "white",
-//           width: "100%",
-//           height: "100%",
-//           display: "flex",
-//           alignItems: "center",
-//           justifyContent: "center",
-//         }}
-//       >
-//         Lift Performance
-//       </div>
-//     ),
-//     {
-//       ...size,
-//     },
-//   );
-// }
+export default function og() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Lift Performance
+      </div>
+    ),
+    {
+      ...size,
+    },
+  );
+}
