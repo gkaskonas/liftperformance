ead57be9ffd8815dffa62d4260ea15c120e51535
Giedrius Kaskonas <pkpersonal@protonmail.com>
2023-05-19 17:08:10 +0800

    fix: lazyload booking script

--- app/components/booking.tsx
+++ app/components/booking.tsx
@@ -16,6 +16,7 @@ export default function Booking() {
         <Script
           src="https://embed.acuityscheduling.com/js/embed.js"
           type="text/javascript"
+          strategy="lazyOnload"
         ></Script>
       </div>
     </section>
