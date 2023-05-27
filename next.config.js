00baa7c6214d23045db67ee1897272c756fee260
Giedrius Kaskonas <pkpersonal@protonmail.com>
2023-05-27 21:39:23 +0800

    chore: test ssg

--- next.config.js
+++ next.config.js
@@ -5,36 +5,37 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  output: "export",
   env: {
     MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
     MAILCHIMP_AUDIENCE_ID: process.env.MAILCHIMP_AUDIENCE_ID,
     MAILCHIMP_API_SERVER: process.env.MAILCHIMP_API_SERVER,
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=3571000; includeSubDomains; preload',
-          },
-          {
-            key: "Cache-Control",
-            value: "s-maxage=3600"
-          }
-        ],
-      },
-    ]
-  }
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)',
+  //       headers: [
+  //         {
+  //           key: 'X-Frame-Options',
+  //           value: 'DENY',
+  //         },
+  //         {
+  //           key: 'X-Content-Type-Options',
+  //           value: 'nosniff',
+  //         },
+  //         {
+  //           key: 'Strict-Transport-Security',
+  //           value: 'max-age=3571000; includeSubDomains; preload',
+  //         },
+  //         {
+  //           key: "Cache-Control",
+  //           value: "s-maxage=3600"
+  //         }
+  //       ],
+  //     },
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
