0adbd966d4b890db44e09ed6a132a4385b07397f
Giedrius Kaskonas <pkpersonal@protonmail.com>
2023-05-27 20:45:03 +0800

    fix: update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=3571000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ]
   }
 }
 
