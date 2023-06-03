017e017d0a3bdf962b81c5651ce813f8843ec64c
Giedrius Kaskonas <pkpersonal@protonmail.com>
2023-06-03 11:56:26 +0800

    fix use server actions

--- app/actions/mailchimp.tsx
+++ app/actions/mailchimp.tsx
@@ -1,3 +1,5 @@
+"use server";
+
 import client from "@mailchimp/mailchimp_marketing";
 
 export async function makeMailchimpRequest(email: string): Promise<string> {
@@ -6,11 +8,11 @@ export async function makeMailchimpRequest(email: string): Promise<string> {
   }
 
   client.setConfig({
-    apiKey: process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY,
-    server: process.env.NEXT_PUBLIC_MAILCHIMP_API_SERVER,
+    apiKey: process.env.MAILCHIMP_API_KEY,
+    server: process.env.MAILCHIMP_API_SERVER,
   });
 
-  const AUDIENCE_ID = process.env.NEXT_PUBLIC_MAILCHIMP_AUDIENCE_ID;
+  const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
 
   if (!AUDIENCE_ID) throw new Error("No audience id");
 
--- next.config.js
+++ next.config.js
@@ -2,40 +2,39 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: "export",
-  env: {
-    NEXT_PUBLIC_MAILCHIMP_API_KEY: process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY,
-    NEXT_PUBLIC_MAILCHIMP_AUDIENCE_ID: process.env.NEXT_PUBLIC_MAILCHIMP_AUDIENCE_ID,
-    NEXT_PUBLIC_MAILCHIMP_API_SERVER: process.env.NEXT_PUBLIC_MAILCHIMP_API_SERVER,
+  experimental: {
+    serverActions: true,
   },
-  images: {
-    unoptimized: true,
+  env: {
+    MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
+    MAILCHIMP_AUDIENCE_ID: process.env.MAILCHIMP_AUDIENCE_ID,
+    MAILCHIMP_API_SERVER: process.env.MAILCHIMP_API_SERVER,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=3571000; includeSubDomains; preload',
+          },
+          {
+            key: "Cache-Control",
+            value: "s-maxage=3600"
+          }
+        ],
+      },
+    ]
   }
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/(.*)',
-  //       headers: [
-  //         {
-  //           key: 'X-Frame-Options',
-  //           value: 'DENY',
-  //         },
-  //         {
-  //           key: 'X-Content-Type-Options',
-  //           value: 'nosniff',
-  //         },
-  //         {
-  //           key: 'Strict-Transport-Security',
-  //           value: 'max-age=3571000; includeSubDomains; preload',
-  //         },
-  //         {
-  //           key: "Cache-Control",
-  //           value: "s-maxage=3600"
-  //         }
-  //       ],
-  //     },
-  //   ]
-  // }
 }
 
 module.exports = nextConfig
