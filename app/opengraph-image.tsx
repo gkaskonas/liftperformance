2cd484ac5f5b189e85455ca27a663780d682f5c2
Giedrius Kaskonas <pkpersonal@protonmail.com>
2023-07-01 11:38:35 +0100

    add opengraph image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,32 +1,43 @@
-import { ImageResponse } from "next/server";
-
-export const alt = "Lift Performance";
+import { ImageResponse } from 'next/server'
+ 
+// Route segment config
+export const runtime = 'edge'
+ 
+// Image metadata
+export const alt = 'Lift Performance'
 export const size = {
   width: 1200,
   height: 630,
-};
-export const contentType = "image/png";
-export const runtime = "edge";
+}
+ 
+export const contentType = 'image/png'
+ 
 
-export default function og() {
+ 
+// Image generation
+export default async function Image() {
   return new ImageResponse(
     (
+      // ImageResponse JSX element
       <div
         style={{
           fontSize: 128,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
         Lift Performance
       </div>
     ),
+    // ImageResponse options
     {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
       ...size,
-    },
-  );
+    }
+  )
 }
