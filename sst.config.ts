6ddf20ca85fd5234eda8e99015484f6b4731c542
Giedrius Kaskonas <pkpersonal@protonmail.com>
2023-07-06 18:44:34 +0100

    fix: update domain

--- sst.config.ts
+++ sst.config.ts
@@ -29,10 +29,7 @@ export default {
         },
         customDomain: {
           domainName: "liftperformance.net",
-          isExternalDomain: true,
-          cdk: {
-            certificate: Certificate.fromCertificateArn(stack, "MyCert", "arn:aws:acm:us-east-1:404319983256:certificate/e17d3238-460d-4c16-aed2-b8e9f6c4f4af"),
-          },
+          domainAlias: "www.liftperformance.net",
         },
       });
 
