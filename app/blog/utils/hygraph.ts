4f2b4fb079589572796172e8b52fbd9ba83be38c
Giedrius Kaskonas <pkpersonal@protonmail.com>
2023-09-19 09:14:43 +0100

    fix: update hygraph calls

--- app/blog/utils/hygraph.ts
+++ app/blog/utils/hygraph.ts
@@ -3,6 +3,6 @@ import { GraphQLClient } from "graphql-request";
 export const hygraph = new GraphQLClient("https://api-ap-northeast-1.hygraph.com/v2/clmadu98l05l801uj4xd85odl/master", {
   fetch,
   next: {
-    revalidate: 900,
-  },
+    tags: ["hygraph"],
+  }
 });
